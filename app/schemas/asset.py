0e39b89f216257ab3ffc3f6b9d3a4fe0272d9be0
Juan Pablo Gomez <287847227+jpgomezs@users.noreply.github.com>
2026-06-28 06:29:27 -0600

    Add Event Pydantic schema for EZRentOut reports

--- app/schemas/asset.py
+++ app/schemas/asset.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
 
 class Asset(BaseModel):
     ez_id: int
@@ -8,3 +11,25 @@ class Asset(BaseModel):
     checkout_on: str
     hour_meter: float
     last_order_id: str | None = None
+
+
+class Event(BaseModel):
+    ain: str = Field(validation_alias="Rentouts / Returns - AIN")
+    action_taken_on: datetime = Field(validation_alias="Rentouts / Returns - Action Taken On")
+    action: str = Field(validation_alias="Rentouts / Returns - Action")
+    fuel_percentage: float = Field(validation_alias="Rentouts / Returns - Porcentaje de combustible ")
+    quantity: int = Field(validation_alias="Rentouts / Returns - Quantity")
+    oin: str = Field(validation_alias="Order - Identification Number")
+    actual_usage: int = Field(validation_alias="Order Line Item - Actual Usage")
+    meter_start: float = Field(validation_alias="Order Line Item - Meter Start")
+    meter_end: float = Field(validation_alias="Order Line Item - Meter End")
+    item_id: int = Field(validation_alias="Rentouts / Returns - Item#")
+    order_id: int = Field(validation_alias="Order - Order#")
+    rentout_date: datetime = Field(validation_alias="Order Line Item - Rent Out/Selling Date")
+    return_date: datetime = Field(validation_alias="Order Line Item - Return Date")
+    fuel_capacity: float = Field(validation_alias="Item - Capacidad Combustible Gal")
+    fuel_type: str = Field(validation_alias="Item - Tipo Combustible")
+    item_name: str = Field(validation_alias="Rentouts / Returns - Item Name")
+    meter_reading: float = Field(validation_alias="Item - Rental Meter (Current Value)")
+    expected_return_date: datetime = Field(validation_alias="Rentouts / Returns - Expected Return Date")
+    item_type: str = Field(validation_alias="Item - Item Type")
