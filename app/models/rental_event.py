90c51ff7da0182012570401ecb252ff44ec9d154
Juan Pablo Gomez <287847227+jpgomezs@users.noreply.github.com>
2026-06-30 11:44:42 -0600

    Change rental_meter_ez type from Float to Integer

--- app/models/rental_event.py
+++ app/models/rental_event.py
@@ -33,6 +33,6 @@ class Event(Base):
     fuel_capacity = Column(Float)
     fuel_type = Column(String)
     item_name = Column(String)
-    rental_meter_ez = Column(Float)
+    rental_meter_ez = Column(Integer)
     expected_return_date = Column(DateTime)
     item_type = Column(String)
